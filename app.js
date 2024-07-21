fbd60bf2824cefdafdd38ddf9e776b7a69e64b7c
Okasha Habib <okasha.qavitech786@gmail.com>
2024-07-21 17:09:04 +0500

    changes

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const tableOf = document.getElementById('tableOf');
 form?.addEventListener('submit', event => {
     event.preventDefault();
     ul.innerHTML = '';
-    const valueInput = inputNumber.value;
+    const valueInput = parseInt(inputNumber.value);
     for (let i = 1; i < 11; i++) {
         card?.classList.remove('hidden');
         tableOf.innerText = `Table of ${valueInput} - 10`;
         ul.innerHTML += `
             <li>
                 <div
-                    class="flex items-center p-3 cursor-pointer text-base font-bold text-gray-300 bg-gray-500 rounded-lg bg-gray-50 hover:bg-gray-600 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                    class="flex items-center cursor-pointer p-3 text-base font-bold text-gray-300 bg-gray-500 rounded-lg bg-gray-50 hover:bg-gray-600 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                     <span class="flex-1 ms-3 whitespace-nowrap">${valueInput} x ${i} = ${valueInput * i}</span>
                 </div>
             </li>
--- app.ts
+++ app.ts
@@ -1,21 +1,21 @@
-const inputNumber = document.getElementById("table");
-const form = document.getElementById('form')
-const card = document.getElementById('card')
-const ul = document.getElementById('tableUl')
-const tableOf = document.getElementById('tableOf')
+const inputNumber = document.getElementById("table") as HTMLInputElement;
+const form = document.getElementById('form') as HTMLFormElement
+const card = document.getElementById('card') as HTMLElement
+const ul = document.getElementById('tableUl') as HTMLElement
+const tableOf = document.getElementById('tableOf') as HTMLElement
 
 form?.addEventListener('submit', event => {
     event.preventDefault()
     ul.innerHTML = ''
         
-    const valueInput: number = inputNumber.value
+    const valueInput: number = parseInt(inputNumber.value)
     for(let i = 1; i < 11; i++){  
         card?.classList.remove('hidden')
         tableOf.innerText = `Table of ${valueInput} - 10`;
         ul.innerHTML += `
             <li>
                 <div
-                    class="flex items-center p-3 text-base font-bold text-gray-300 bg-gray-500 rounded-lg bg-gray-50 hover:bg-gray-600 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                    class="flex items-center cursor-pointer p-3 text-base font-bold text-gray-300 bg-gray-500 rounded-lg bg-gray-50 hover:bg-gray-600 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                     <span class="flex-1 ms-3 whitespace-nowrap">${valueInput} x ${i} = ${valueInput * i}</span>
                 </div>
             </li>
