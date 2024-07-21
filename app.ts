61bf8d9af10ef7e099b59871ebfd5d1e7cc0d4e2
Okasha Habib <okasha.qavitech786@gmail.com>
2024-07-21 16:47:38 +0500

    TypeScript multiplication table

---
+++ app.ts
@@ -0,0 +1,25 @@
+const inputNumber = document.getElementById("table");
+const form = document.getElementById('form')
+const card = document.getElementById('card')
+const ul = document.getElementById('tableUl')
+const tableOf = document.getElementById('tableOf')
+
+form?.addEventListener('submit', event => {
+    event.preventDefault()
+    ul.innerHTML = ''
+        
+    const valueInput: number = inputNumber.value
+    for(let i = 1; i < 11; i++){  
+        card?.classList.remove('hidden')
+        tableOf.innerText = `Table of ${valueInput} - 10`;
+        ul.innerHTML += `
+            <li>
+                <a href="#"
+                    class="flex items-center p-3 text-base font-bold text-gray-300 bg-gray-500 rounded-lg bg-gray-50 hover:bg-gray-600 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                    <span class="flex-1 ms-3 whitespace-nowrap">${valueInput} x ${i} = ${valueInput * i}</span>
+                </a>
+            </li>
+        `
+    }
+    form.reset()
+})
